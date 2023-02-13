01bc53d36d1b23724d547e9d0ac5865133ba57a9
Ban Alex <alexbanut10@gmail.com>
2023-02-13 22:33:33 +0200

    section 6 MySQL connectivity with PDO, lecture 1

---
+++ section6_PDO/PDO-SQL.php
@@ -0,0 +1,25 @@
+<?php
+echo "<pre>\n";
+$pdo=new PDO('mysql:host=localhost;port=4307;dbname=misc',
+     'fred', 'zap');
+$stmt = $pdo->query("SELECT * FROM users");
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+print_r($rows);
+echo "</pre>\n";
+?>
+
+<!-- Output : 
+Array(
+    [user_id] => 1
+    [name] => Chuck
+    [email] => csev@umich.edu
+    [password] => 123
+)
+
+Array(
+    [user_id] => 2
+    [name] => Glenn
+    [email] => gg@umich.edu
+    [password] => 456
+)
+-->
