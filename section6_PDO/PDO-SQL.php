5cc477635585da4dcb0962b3b9679186c0351c80
Ban Alex <alexbanut10@gmail.com>
2023-02-14 08:57:56 +0200

    pdo connection error

--- section6_PDO/PDO-SQL.php
+++ section6_PDO/PDO-SQL.php
@@ -1,6 +1,6 @@
 <?php
 echo "<pre>\n";
-$pdo=new PDO('mysql:host=localhost;port=4307;dbname=misc',
+$pdo=new PDO('mysql:host=127.0.1;port=4307;dbname=misc',
      'fred', 'zap');
 $stmt = $pdo->query("SELECT * FROM users");
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
--- section6_PDO/PDO_MySQL_demo/pdo.php
+++ section6_PDO/PDO_MySQL_demo/pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=4307;dbname=misc',
+$pdo = new PDO('mysql:host=127.0.0.1;port=4307;dbname=misc',
     'fred', 'zap');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
